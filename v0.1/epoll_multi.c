076e30ee38c6373f19ab8efed5d53cca74a82b92
jang <2235908312@qq.com>
2024-02-06 00:38:42 +0800

    one accept multi read by epoll with some race bug

--- v0.1/epoll_multi.c
+++ v0.1/epoll_multi.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 #include <pthread.h>
-#include <sys/select.h>
 
 #include <assert.h>
 
+#include <sys/epoll.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
@@ -17,8 +18,9 @@
 #define handle_error(msg) \
 	do { perror(msg); exit(EXIT_FAILURE); } while (0)
 #define P_NUMS 2
+#define MAX_EVENTS 10
 
-void * handler_accept(int sfd, fd_set *readFds) {
+void * handler_accept(int epollfd, int sfd) {
 
 	struct sockaddr_in peer_addr;
 	socklen_t peer_addr_len = sizeof(peer_addr);
@@ -30,25 +32,33 @@ void * handler_accept(int sfd, fd_set *readFds) {
 		return NULL;
 	char ip[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &(peer_addr.sin_addr), ip, INET_ADDRSTRLEN);
-	FD_SET(cfd, readFds);
+
+	struct epoll_event ev;
+	ev.events = EPOLLIN;
+	ev.data.fd = cfd;
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, cfd, &ev) == -1) 
+		handle_error("epoll_ctl");
+
 	printf("tid : <%lu> fd : <%d> : Contecting to %s : %hu.\n", pthread_self(), cfd, ip, ntohs(peer_addr.sin_port));
 }
 
 void * handler_read(void *arg) {
 	void *(*argv)[2]  = (void *(*)[2]) arg;
 	int cfd = *((int *) (*argv)[0]);
-	fd_set *readFds = ((fd_set *) (*argv)[1]);
+	int epollfd = *((int *) (*argv)[1]);
 
-		char buf[1024];
-		int recv_len = recv(cfd, buf, sizeof(buf), 0);
-		buf[recv_len] = '\0';
-		printf("%s", buf);
-		printf("recv = %d\n", recv_len);
-		//int send_len = send(cfd, buf, recv_len, 0);
+	//printf("cfd = %d ; epollfd = %d\n", cfd, epollfd);
+	char buf[1024];
+	int recv_len = recv(cfd, buf, sizeof(buf), 0);
+	buf[recv_len] = '\0';
+	printf("%s", buf);
+	//printf("recv = %d\n", recv_len);
+	//int send_len = send(cfd, buf, recv_len, 0);
 		
 
 	printf("tid : <%lu> fd : <%d> : Close connection.\n", pthread_self(), cfd);
-	FD_CLR(cfd, readFds);
+	//if (epoll_ctl(epollfd, EPOLL_CTL_DEL, cfd, NULL) == -1) 
+	//	handle_error("epoll_ctl");
 	close(cfd);
 	free((*argv)[0]);
 }
@@ -76,35 +86,45 @@ int main (int argc, char*argv[]) {
 	struct sockaddr_in peer_addr;
 	socklen_t peer_addr_len = sizeof(peer_addr);
 
+	int epollfd = epoll_create1(0);
+	if (epollfd == -1) 
+		handle_error("epoll_create1");
+	struct epoll_event ev, events[MAX_EVENTS];
+	//init event
+	ev.events = EPOLLIN;
+	ev.data.fd = sfd;
+
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sfd, &ev) == -1) 
+		handle_error("epoll_ctl");
+
+
 	while (1) {
-		int max_fd = 8;
-		fd_set readFds;
-		FD_ZERO(&readFds);
-		FD_SET(sfd, &readFds);
-
-		int rc = select(1024, &readFds, NULL, NULL, NULL);
-
-		for (int i = sfd; i < 1024; ++ i) {
-			if (i == sfd && FD_ISSET(i, &readFds)) {
+		int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+		if (nfds == -1) 
+			handle_error("epoll_wait");
+		for (int i = 0; i < nfds; ++ i) {
+			if (events[i].data.fd == sfd) {
 				printf("accept\n");
-				handler_accept(sfd, &readFds);
-			}
-			if (i != sfd && FD_ISSET(i, &readFds)) {
-				printf("read i = %d\n", i);
+				handler_accept(epollfd, sfd);
+			}else {
+				printf("read fd = %d\n", events[i].data.fd);
 				void *argv[2];
 				int *rfd = (int *) malloc(sizeof (int));
-				*rfd = i;
+				*rfd = events[i].data.fd;
 				argv[0] = rfd;
-				argv[1] = &readFds;
-				printf("read\n");
+				argv[1] = &epollfd;
 				pthread_t p2;
+				if (epoll_ctl(epollfd, EPOLL_CTL_DEL, *rfd, NULL) == -1) 
+					handle_error("epoll_ctl");
 				int pret = pthread_create(&p2, NULL, handler_read, argv);
 				if (pret != 0)
 					handle_error("pthread creat");
 				printf("Creat a read pthread <%ld>.\n", p2);
+				//sleep(1);
 
 			}
 		}
+
 	}
 
 	printf("All have finished\n");
